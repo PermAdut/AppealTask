9148acd2974297fbc908d96a3f2004d67b11f4be
вфывфы <hdvhdvwww@gmail.com>
2025-04-24 22:22:48 +0300

    fix: can be find by direct date

--- src/middleware/parseQuery.ts
+++ src/middleware/parseQuery.ts
@@ -3,8 +3,14 @@ import { Request, Response, NextFunction } from 'express';
 export function parseQuery(req: Request, res: Response, next: NextFunction) {
   try {
     const dates = req.query.date as string[];
-    if (!dates || dates.length !== 2) {
-      throw new Error('You must provide two dates');
+    if (!dates) {
+      throw new Error('Invalid date format, use YYYY-MM-DD');
+    }
+    if (dates.length === 1) {
+        dates.push(dates[0]);
+    }
+    if (dates.length !== 2) {
+      throw new Error('You must provide one or two dates');
     }
     dates.forEach((el) => {
       const regex = /^\d{4}-\d{2}-\d{2}$/;
@@ -18,7 +24,7 @@ export function parseQuery(req: Request, res: Response, next: NextFunction) {
     });
     next();
   } catch (error: any) {
-    if (error.message === 'You must provide two dates') {
+    if (error.message === 'You must provide one or two dates') {
       res.status(400).json({ message: error.message });
     } else if (error.message === 'Invalid date format, use YYYY-MM-DD') {
       res.status(400).json({ message: error.message });
